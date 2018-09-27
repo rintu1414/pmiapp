3a035577c865342a8ee278b9e2ab694e13c30bc7
Gantayat <email@example.com>
2018-09-26 20:28:20 -0400

    New

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,34 +11,34 @@ import {Observable} from 'rxjs/internal/Observable';
   styleUrls: ['./header.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit/*, OnDestroy*/ {
   currentUser: User;
-  navbarOpen = false;
-  mobileQuery: MediaQueryList;
+/*  navbarOpen = false;
+  mobileQuery: MediaQueryList;*/
   isLoggedIn$: Observable<boolean>;
 
-  private _mobileQueryListener: () => void;
-  constructor(public authService: AuthServiceService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  // private _mobileQueryListener: () => void;
+  constructor(public authService: AuthServiceService /*changeDetectorRef: ChangeDetectorRef, media: MediaMatcher*/) {
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+  /*  this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
+    this.mobileQuery.addListener(this._mobileQueryListener);*/
   }
   ngOnInit() {
     this.isLoggedIn$ = this.authService.isLoggedIn; // {2}
   }
 
-  ngOnDestroy(): void {
+/*  ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
+  }*/
 // login out from the app
   logOut() {
     this.authService.logout();
   }
 
-  toggleNavbar() {
+/*  toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
-  }
+  }*/
 
 
 }
