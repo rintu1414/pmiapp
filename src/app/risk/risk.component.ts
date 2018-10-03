e85a93888118e5e8a39e49fd9f8f10aca3eeabfd
Ankur Bhatia <AnkMac@Ankurs-MacBook-Pro.local>
2018-10-02 22:24:42 -0400

    export

--- src/app/risk/risk.component.ts
+++ src/app/risk/risk.component.ts
@@ -51,7 +51,7 @@ export class RiskComponent implements OnInit {
   }
   ExportTOExcel() {
     console.log('export');
-    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(this.table.nativeElement);
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.dataSource.filteredData);
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
 
