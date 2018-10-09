805bf5cdbd35cb84382508e639f47f49277b82e3
Gantayat <email@example.com>
2018-10-08 23:34:31 -0400

    pie chart

--- src/app/risk/risk.component.ts
+++ src/app/risk/risk.component.ts
@@ -17,34 +17,61 @@ export class RiskComponent implements OnInit {
   file: File;
   risk: Risk[];
   loading  =  true;
+  myData = [
+    ['Resource', 3],
+    ['Procurement', 2],
+    ['Technology', 5],
+    ['Process', 3]
+  ];
 
-  rank: any = {
-    'Rare#Insignificant': '1',
-    'Unlikely#Insignificant': '2',
-    'Possible#Insignificant': '3',
-    'Likely#Insignificant': '4',
-    'Rare#Minor': '5',
-    'Unlikely#Minor': '6',
-    'Certain#Insignificant': '7',
-    'Possible#Minor': '8',
-    'Likely#Minor': '9',
-    'Certain#Minor': '10',
-    'Rare#Moderate': '11',
-    'Rare#Major': '12',
-    'Unlikely#Moderate': '13',
-    'Possible#Moderate': '14',
-    'Likely#Moderate': '15',
-    'Certain#Moderate': '16',
-    'Unlikely#Major': '17',
-    'Possible#Major': '18',
-    'Rare#Catastrophic': '19',
-    'Likely#Major': '20',
-    'Certain#Major': '21',
-    'Unlikely#Catastrophic': '22',
-    'Possible#Catastrophic': '23',
-    'Likely#Catastrophic': '24',
-    'Certain#Catastrophic': '25'
+  columnNames = ['Category', 'Total'];
+  roles = [];
+  title: 'Patterns',
+  type = 'PieChart';
+  columnNamesArea: any = ['Year', 'Sales', 'Expenses'];
+  areatitle = 'Response';
+  areaType = 'AreaChart';
+
+
+  rank: any = {'1': {'color': 'yellow', 'data': {}},
+    '2': {'color': 'yellow', 'data': {}},
+    '3': {'color': 'yellow', 'data': {}},
+    '4': {'color': 'amber', 'data': {}},
+    '5': {'color': 'amber', 'data': {}},
+    '6': {'color': 'amber', 'data': {}},
+    '7': {'color': 'amber', 'data': {}},
+    '8': {'color': 'amber', 'data': {}},
+    '9': {'color': 'amber', 'data': {}},
+    '10': {'color': 'amber', 'data': {}},
+    '11': {'color': 'golden', 'data': {}},
+    '12': {'color': 'golden', 'data': {}},
+    '13': {'color': 'golden', 'data': {}},
+    '14': {'color': 'golden', 'data': {}},
+    '15': {'color': 'golden', 'data': {}},
+    '16': {'color': 'golden', 'data': {}},
+    '17': {'color': 'golden', 'data': {}},
+    '18': {'color': 'golden', 'data': {}},
+    '19': {'color': 'golden', 'data': {}},
+    '20': {'color': 'red', 'data': {}},
+    '21': {'color': 'red', 'data': {}},
+    '22': {'color': 'red', 'data': {}},
+    '23': {'color': 'red', 'data': {}},
+    '24': {'color': 'red', 'data': {}},
+    '25': {'color': 'red', 'data': {}}
   };
+
+  ta: any = [['11', '16', '20', '23', '25'],
+    ['7', '12', '17', '21', '24'],
+    ['4', '8', '13', '18', '22'],
+    ['2', '5', '9', '14', '19'],
+    ['1', '3', '6', '10', '15']];
+
+  areaData: any =  [
+    ['2013', 1000, 400],
+    ['2014', 1170, 460],
+    ['2015', 660, 1120],
+    ['2016', 1030, 540]
+  ];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -87,5 +114,23 @@ export class RiskComponent implements OnInit {
     XLSX.writeFile(wb, 'SheetJS.xlsx');
     console.log('exported');
 
+  }
+  getCss(i: String) {
+    return this.rank[i].color;
+  }
+  getPieChartData() {
+    const pieData: any = new Map();
+    if (this.dataSource) {
+    this.dataSource.data.forEach((task) => {
+     if (pieData.has(task.riskCategory)) {
+       const va = pieData.get(task.riskCategory) + 1;
+        pieData.set(task.riskCategory, va);
+      }
+      else {
+        pieData.set(task.riskCategory, 1);
+
+      }
+    }); }
+    return pieData;
   }
 }
